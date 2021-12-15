82c50a36ff3f8076d420eb83a110aecbbcd5484c
perkles <maikosilvae@gmail.com>
2021-12-15 14:19:41 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,63 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private string nome;
+    private string cor;
+    private interger idade;
+
+    public Gato() {}
+
+    public Gato(string nome, string cor, interger idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public string getCor() {
+        return cor;
+    }
+
+    public void setCor(string cor) {
+        this.cor = cor;
+    }
+
+    public interger getIdade() {
+        return idade;
+    }
+
+    public void setIdade(interger idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome=" + nome +
+                ", cor=" + cor +
+                ", idade=" + idade +
+                '}';
+    }
+}
