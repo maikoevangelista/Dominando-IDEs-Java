10e120b4220d63008bb8180d6264a1b22a826e97
perkles <maikosilvae@gmail.com>
2021-12-15 14:48:51 -0300

    Class livro

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -7,8 +7,45 @@ public class primeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("o problema dos 3 corpos","300");
+        system.out.println(livro1);
+
         //int a = 5;
         //int b = 3;
         //System.out.println("Hello world! " + (a+b));
+    }
+}
+
+class Livro {
+    private string nome;
+    private integer numPaginas;
+
+    public Livro(string nome, integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome=" + nome +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
