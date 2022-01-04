18abca19da1b6f6b5a817c15fb2db99e3537bbc0
perkles <maikosilvae@gmail.com>
2022-01-03 22:13:55 -0300

    Class livro 4

---
+++ src/br/com/dio/calculadora/caclculadora.java
@@ -0,0 +1,35 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class caclculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a,b;
+
+        System.out.println("digite o primeiro valor: ");
+        za = scan,nextInt();
+        System.out.println("digite o segundo valor: ");
+        b = scan,nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        in multiplicar = multiplicar(a, b);
+
+
+    }
+
+    public static in somar(int a,int b) {
+        return a - b;
+    }
+    public static in subtrair(int a,int b) {
+        return a - b;
+    }
+    public static in multiplicar(int a,int b) {
+        return a * b;
+    }
+    public static in dividir(int a,int b) {
+        return a / b;
+    }
+}
--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -63,3 +63,6 @@ public class Gato {
                 '}';
     }
 }
+
+
+///
